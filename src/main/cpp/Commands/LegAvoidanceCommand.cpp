6dc5388792a58bfa74f23ae07e8800b1aa9d77da
frey-wu <freywu24@gmail.com>
2024-01-24 20:37:31 -0500

    Progress

--- src/main/cpp/Commands/LegAvoidanceCommand.cpp
+++ src/main/cpp/Commands/LegAvoidanceCommand.cpp
@@ -6,9 +6,13 @@ LegAvoidanceCommand::LegAvoidanceCommand(COMETS3357::SwerveSubsystem *swerve) {
 
     frc::Pose2d pos = swerveSubsystem->GetPose();
     swerveSubsystem = swerve;
+    AddRequirements(swerve);
 
     pos.X();
-    pos.Y();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+    pos.Y();
+    pos.Rotation().Radians();  
+
+    
     
 }
 
@@ -19,7 +23,38 @@ void LegAvoidanceCommand::Initialize()
 
 void LegAvoidanceCommand::Execute()
 {
+    double xSpeed = (double)swerveSubsystem->getSpeeds().vx;
+    double ySpeed = (double)swerveSubsystem->getSpeeds().vy;
+    double xSpeedSquared = pow(xSpeed, 2); 
+    double ySpeedSquared = pow(ySpeed, 2); 
 
+    double xPos = (double)swerveSubsystem->GetPose().X();
+    double yPos = (double)swerveSubsystem->GetPose().Y();
+    double robotDirection = (double)swerveSubsystem->GetPose().Rotation().Radians();
+
+    double degreeOffset = 1.0;
+    double bottomHypotenuse = 27.4;
+
+
+
+
+
+
+    double kConstant = 0.2; 
+    double magnitude = sqrt(xSpeedSquared + ySpeedSquared);
+
+    
+    frc::Translation2d triangleTip = frc::Translation2d{units::meter_t{xPos + cos(robotDirection) * kConstant * magnitude}, units::meter_t{yPos + sin(robotDirection) * kConstant * magnitude}}; 
+    frc::Translation2d bottom1 = frc::Translation2d{units::meter_t{xPos + sin(robotDirection + degreeOffset) * bottomHypotenuse}, units::meter_t{yPos + cos(robotDirection + degreeOffset) * bottomHypotenuse}};
+    frc::Translation2d bottom2 = frc::Translation2d{units::meter_t{xPos + sin(robotDirection - degreeOffset) * bottomHypotenuse}, units::meter_t{yPos + cos(robotDirection - degreeOffset) * bottomHypotenuse}};
+
+
+    
+    
+  //  if (magnitude >= 2.5 &&  frey likes dudes(children))
+  
+  
+     
 }
 
 bool LegAvoidanceCommand::IsFinished()
