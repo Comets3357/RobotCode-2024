6e98562a840d8c69a70d537925d7cc5202f74a72
Aiden Platt <aidenplatt0202@gmail.com>
2024-01-25 19:51:40 -0500

    hello world

--- src/main/cpp/Commands/LegAvoidanceCommand.cpp
+++ src/main/cpp/Commands/LegAvoidanceCommand.cpp
@@ -33,7 +33,7 @@ void LegAvoidanceCommand::Execute()
     double robotDirection = (double)swerveSubsystem->GetPose().Rotation().Radians();
 
     double degreeOffset = 1.0;
-    double bottomHypotenuse = 27.4;
+    double bottomHypotenuse = 0.7; 
 
     double xPosBottom1 = xPos + sin(robotDirection + degreeOffset) * bottomHypotenuse;
     double yPosBottom1 = yPos + cos(robotDirection + degreeOffset) * bottomHypotenuse;
@@ -45,14 +45,31 @@ void LegAvoidanceCommand::Execute()
 
 
 
-    double kConstant = 0.2; 
+    double kConstant = .5; 
     double magnitude = sqrt(xSpeedSquared + ySpeedSquared);
 
+
+    double sideLengthB = sqrt(0.7 * 0.7 + pow(magnitude * kConstant,2)); 
+    double sideLengthC = sideLengthB; 
+
+    frc::Translation2d redA = frc::Translation2d{units::meter_t{3.3}, units::meter_t{3.05}}; 
+    frc::Translation2d redB = frc::Translation2d{units::meter_t{3.3}, units::meter_t{3.05}}; 
+    frc::Translation2d redC = frc::Translation2d{units::meter_t{3.3}, units::meter_t{3.05}}; 
+
+    frc::Translation2d blueA = frc::Translation2d{units::meter_t{3.3}, units::meter_t{3.05}}; 
+    frc::Translation2d blueB = frc::Translation2d{units::meter_t{3.3}, units::meter_t{3.05}}; 
+    frc::Translation2d blueC = frc::Translation2d{units::meter_t{3.3}, units::meter_t{3.05}}; 
     
     frc::Translation2d triangleTip = frc::Translation2d{units::meter_t{xPos + cos(robotDirection) * kConstant * magnitude}, units::meter_t{yPos + sin(robotDirection) * kConstant * magnitude}}; 
     frc::Translation2d bottom1 = frc::Translation2d{units::meter_t{xPosBottom1}, units::meter_t{yPosBottom1}};
     frc::Translation2d bottom2 = frc::Translation2d{units::meter_t{xPosBottom2}, units::meter_t{yPosBottom2}};
-    frc::Translation2d bottom1extension = frc::Translation2d{}
+    frc::Translation2d bottom1extension = frc::Translation2d{}; 
+
+
+
+    
+    
+
 
 
     
