ae2f176c464aceb617d473806f71623323ff5b71
Zachary Sanford <ZacharySanford@outlook.com>
2024-02-24 11:48:51 -0500

    Code
    Fixed code for LED's

--- src/main/cpp/Subsystems/LEDsSubsystem.cpp
+++ src/main/cpp/Subsystems/LEDsSubsystem.cpp
@@ -8,8 +8,8 @@ LEDsSubsystem::LEDsSubsystem() : COMETS3357::Subsystem("LEDsSubsystem") {
 
 
 void LEDsSubsystem::writeLEDs(int g, int r, int b) {
-    greenPub.Set(g);
     redPub.Set(r);
+    greenPub.Set(g);
     bluePub.Set(b);
 
     
@@ -17,9 +17,8 @@ void LEDsSubsystem::writeLEDs(int g, int r, int b) {
 
 void LEDsSubsystem::Initialize()
 {
-   
-    greenPub = table->GetIntegerTopic("gValue").Publish();
     redPub = table->GetIntegerTopic("rValue").Publish();
+    greenPub = table->GetIntegerTopic("gValue").Publish();
     bluePub = table->GetIntegerTopic("bValue").Publish();
 
 }
@@ -28,23 +27,23 @@ void LEDsSubsystem::Periodic() {
     while (!enabled) {
         if (gyroZero)
         {
-            LEDsSubsystem::writeLEDs(8,255,0); // orange
-            greenPub = table->GetIntegerTopic("gValue").Publish();
+            LEDsSubsystem::writeLEDs(100,255,0); // orange
             redPub = table->GetIntegerTopic("rValue").Publish();
+            greenPub = table->GetIntegerTopic("gValue").Publish();
             bluePub = table->GetIntegerTopic("bValue").Publish();
         } 
         else if (comms)
         {
             LEDsSubsystem::writeLEDs(255,0,0); // green
-            greenPub = table->GetIntegerTopic("gValue").Publish();
             redPub = table->GetIntegerTopic("rValue").Publish();
+            greenPub = table->GetIntegerTopic("gValue").Publish();
             bluePub = table->GetIntegerTopic("bValue").Publish();
         } 
         else 
         {
             LEDsSubsystem::writeLEDs(0,255,0); // red 
-            greenPub = table->GetIntegerTopic("gValue").Publish();
             redPub = table->GetIntegerTopic("rValue").Publish();
+            greenPub = table->GetIntegerTopic("gValue").Publish();
             bluePub = table->GetIntegerTopic("bValue").Publish();
         } 
         
@@ -59,36 +58,36 @@ void LEDsSubsystem::Periodic() {
         if (ampSignal)
         {
             LEDsSubsystem::writeLEDs(255,255,0); // yellow // signal to activate amp
-            greenPub = table->GetIntegerTopic("gValue").Publish();
             redPub = table->GetIntegerTopic("rValue").Publish();
+            greenPub = table->GetIntegerTopic("gValue").Publish();
             bluePub = table->GetIntegerTopic("bValue").Publish();
         } 
         else if (hpSignal)
         {
             LEDsSubsystem::writeLEDs(0,255,255); // purple // signal to the human player // need to make flashing
-            greenPub = table->GetIntegerTopic("gValue").Publish();
             redPub = table->GetIntegerTopic("rValue").Publish();
+            greenPub = table->GetIntegerTopic("gValue").Publish();
             bluePub = table->GetIntegerTopic("bValue").Publish();
         }
         else if (mode == "XBOXManual") 
         {
-            LEDsSubsystem::writeLEDs(255,0,255); // teal // manual mode
-            greenPub = table->GetIntegerTopic("gValue").Publish();
+            LEDsSubsystem::writeLEDs(128,0,128); // teal // manual mode
             redPub = table->GetIntegerTopic("rValue").Publish();
+            greenPub = table->GetIntegerTopic("gValue").Publish();
             bluePub = table->GetIntegerTopic("bValue").Publish();
         } 
-        // else if (IndexerSubsystem::IsDetected) 
-        // {
-        //     LEDsSubsystem::writeLEDs(255,0,0); // green // game piece detection
-        //     greenPub = table->GetIntegerTopic("gValue").Publish();
-        //     redPub = table->GetIntegerTopic("rValue").Publish();
-        //     bluePub = table->GetIntegerTopic("bValue").Publish();
-        // } 
-        else 
+        else if (IndexerSubsystem::IsDetected) 
         {
-            LEDsSubsystem::writeLEDs(8,255,0); // orange // default mode // semi-auto
-            greenPub = table->GetIntegerTopic("gValue").Publish();
+            LEDsSubsystem::writeLEDs(255,0,0); // green // game piece detection
             redPub = table->GetIntegerTopic("rValue").Publish();
+            greenPub = table->GetIntegerTopic("gValue").Publish();
+            bluePub = table->GetIntegerTopic("bValue").Publish();
+        } 
+        else
+        {
+            LEDsSubsystem::writeLEDs(100,255,0); // orange // default mode // semi-auto
+            redPub = table->GetIntegerTopic("rValue").Publish();
+            greenPub = table->GetIntegerTopic("gValue").Publish();
             bluePub = table->GetIntegerTopic("bValue").Publish();
         } 
     }
