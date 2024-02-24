244e28047a21bf583df7c41d571b88d9d238d77e
Hendrik <hnsorens@gmail.com>
2024-02-24 14:32:50 -0500

    Fixed somethingf

--- src/main/cpp/Subsystems/AutonResetGyroButtonSubsystem.cpp
+++ src/main/cpp/Subsystems/AutonResetGyroButtonSubsystem.cpp
@@ -1,4 +1,5 @@
 #include "Subsystems/AutonResetGyroButtonSubsystem.h"
+#include <frc/smartdashboard/SmartDashboard.h>
 
 AutonGyroResetSubsystem::AutonGyroResetSubsystem(frc2::InstantCommand* gyroReset) : COMETS3357::Subsystem("AutonGyroResetSubsystem") {
     resetCommand = gyroReset;
@@ -10,8 +11,9 @@ void AutonGyroResetSubsystem::Initialize()
 }
 
 void AutonGyroResetSubsystem::Periodic() {
-    if (resetButton.Get())
-    {
-        resetCommand->Schedule();
-    }
+    frc::SmartDashboard::PutBoolean("Button", resetButton.Get());
+    // if (!resetButton.Get())
+    // {
+    //     resetCommand->Schedule();
+    // }
 }
