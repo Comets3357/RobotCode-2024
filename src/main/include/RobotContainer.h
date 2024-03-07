d37e89cd3626b4b814321997c4ec710c8efb8f98
Aiden Platt <aidenplatt0202@gmail.com>
2024-03-06 20:46:06 -0500

    For Hendrick to Fix

--- src/main/include/RobotContainer.h
+++ src/main/include/RobotContainer.h
@@ -123,8 +123,8 @@ class RobotContainer {
   frc2::InstantCommand piece4AutoSetpoint3{[this](){shooter.SetPositionPivot(40.5), shooter.SetVelocityKickerWheel(2000); shooter.SetVelocityFlyWheel(-2000);}, {}};
   frc2::InstantCommand piece4AutoSetpoint4{[this](){shooter.SetPositionPivot(28), shooter.SetVelocityKickerWheel(3000); shooter.SetVelocityFlyWheel(-3000);}, {}};
   frc2::InstantCommand midPiece4AutoSetpoint{[this](){shooter.SetPositionPivot(29), shooter.SetVelocityKickerWheel(2500); shooter.SetVelocityFlyWheel(-2500);}, {}};
-  AutonPathCommand ampAlignBlue{&swerve, 0.5, 5, frc::Pose2d::Pose2d{frc::Translation2d(units::meter_t{1.85}, units::meter_t{7.79}), frc::Rotation2d{units::degree_t{-90}}}};
-  AutonPathCommand ampAlignRed{&swerve, 0.5, 5, frc::Pose2d::Pose2d{frc::Translation2d(units::meter_t{14.68}, units::meter_t{7.79}), frc::Rotation2d{units::degree_t{-90}}}};
+  AutonPathCommand ampAlignBlue{&swerve, 0.5, .5, frc::Pose2d{frc::Translation2d(units::meter_t{1.85}, units::meter_t{7.79}), frc::Rotation2d{units::radian_t{1.57}}}, true};
+  AutonPathCommand ampAlignRed{&swerve, 0.5, .5, frc::Pose2d{frc::Translation2d(units::meter_t{14.68}, units::meter_t{7.79}), frc::Rotation2d{units::radian_t{1.57}}}, true};
   frc2::InstantCommand ampStart{[this](){
     if (frc::DriverStation::GetAlliance() == frc::DriverStation::Alliance::kRed)
     {
